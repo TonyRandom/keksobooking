7eee5525906b5ebeb0bb21253357b53c099d82f4
TonyRandom <65846634+TonyRandom@users.noreply.github.com>
2020-11-11 09:39:52 +0300

    dragndrop

--- js/map.js
+++ js/map.js
@@ -612,3 +612,48 @@ roomNumberField.addEventListener('click', function () {
 	}
 });
 
+
+
+// MODULE 5
+
+/*  <button class="map__pin map__pin--main">
+        <img src="img/main-pin-image.png" width="40" height="44" draggable="false"> */
+
+
+/* mapPinMain.addEventListener('mousedown', function(evt) {
+	console.log(evt.target);
+	evt.preventDefault();
+
+	var startCoords = {
+		x: evt.clientX,
+		y: evt.clientY
+	};
+	
+	var onMouseMove = function (moveEvt) {
+		moveEvt.preventDefault();
+		var shift = {
+			x: startCoords.x - moveEvt.clientX,
+			y: startCoords.y - moveEvt.clientY
+		}
+
+		startCoords = {
+			x: moveEvt.clientX,
+			y: moveEvt.clientY
+		} 
+
+		mapPinMain.style.top = (mapPinMain.offsetLeft - shift.x) + 'px';
+		mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
+	}
+
+	var onMouseUp = function (upEvt) {
+		upEvt.preventDefault();
+		document.removeEventListener('mousemove', onMouseMove);
+		document.removeEventListener('mouseup', onMouseUp);
+	   };
+
+	
+
+	document.addEventListener('mousemove', onMouseMove);
+	document.removeEventListener('mouseup', onMouseUp);
+
+}) */
